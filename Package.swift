730a6373ac40c10c554f655415c1bfdbb67eff9f
Ben Stovold <bstovold@gmail.com>
2019-10-18 11:27:53 +1000

    Re-point dependency to repo that supports SPM

--- Package.swift
+++ Package.swift
@@ -59,7 +59,7 @@ let package = Package(
         .library(name: "RMQClient", targets: ["RMQClient"])
         ],
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.3"),
+        .package(url: "https://github.com/grangej/CocoaAsyncSocket.git", from: "master"),
         .package(url: "https://github.com/jeffh/JKVValue.git", from: "1.3.3")
     ],
     targets: [
